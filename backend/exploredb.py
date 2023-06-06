58e2a095ce14d027f4fdc451fab961d8977e281f
Eman <eacosta@roundabout.com>
2023-06-06 13:28:20 -0700

    Updated db submod

--- backend/exploredb.py
+++ backend/exploredb.py
@@ -1,4 +1,5 @@
 from urllib.parse import parse_qs
+from sqlalchemy import MetaData
 
 from db import utils
 from db.schema import Spec, Client, TempSpec
@@ -7,12 +8,9 @@ from db.config import SESSIONFACTORY, ENGINE
 # Manual update of sequence to highest ID when migrating to new schema
 # SELECT setval(pg_get_serial_sequence('your_table', 'id'), (SELECT MAX(id) FROM your_table));
 
-session = SESSIONFACTORY()
-spec  = session.query(Spec).filter(Spec.name == "TEST RE-SUBMIT CLIENT ").first()
-if spec is None:
-    session.close()
-    exit()
-print(spec.name)
-session.delete(spec)
-session.commit()
-session.close()
+# session = SESSIONFACTORY()
+# allspecs = session.query(Spec).all()
+# print(allspecs)
+# session.close()
+
+utils.init_db()
